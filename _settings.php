c9d8cb614e30b4d0092ade735964eee54603a218
Alexandre Rodriguez <alexandre@identiq.net>
2020-12-02 03:10:15 +0100

    work

--- _settings.php
+++ _settings.php
@@ -3,6 +3,8 @@
 $settings = '';
 
 $settings['dbhost'] = getenv("MYSQL_ADDON_HOST");
+$settings['dbport'] = getenv("MYSQL_ADDON_PORT");
+
 $settings['dbname'] = getenv("MYSQL_ADDON_DB");
 $settings['dbusername'] = getenv("MYSQL_ADDON_USER");
 $settings['dbpassword'] = getenv("MYSQL_ADDON_PASSWORD");
@@ -12,12 +14,14 @@ $settings['mjusername'] = getenv("MAILJET_USERNAME");
 $settings['mjpassword'] = getenv("MAILJET_PASSWORD");
 $settings['mjfrom'] = "info@aquavelo.com";
 
-/*
 
-$database = new PDO("mysql:host=" . $settings['dbhost'] . ";dbname=" . $settings['dbname'], $settings['dbusername'], $settings['dbpassword']);
-*/
-               /*
-               $database = new PDO("mysql:host=".$settings['dbhost'].";dbname=".$settings['dbname'].";charset=utf8", $settings['dbusername'], $settings['dbpassword'],
-               array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-               */
-?>
+try {
+        $database = new PDO(
+                "mysql:host=" . $settings['dbhost'] . ";port=" . $settings['dbport'] . ";dbname=" . $settings['dbname'] . ";charset=utf8",
+                $settings['dbusername'],
+                $settings['dbpassword'],
+                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")
+        );
+} catch (PDOException $e) {
+        echo $e->getMessage();
+}
--- pdo.php
+++ pdo.php
@@ -3,9 +3,17 @@ try {
   $database = new PDO(
     'mysql:host=' . getenv("MYSQL_ADDON_DIRECT_HOST") . ';port' . getenv('MYSQL_ADDON_DIRECT_PORT') . ';dbname=' . getenv("MYSQL_ADDON_DB"),
     getenv("MYSQL_ADDON_USER"),
-    getenv("MYSQL_ADDON_PASSWORD")
+    getenv("MYSQL_ADDON_PASSWORD"),
   );
 } catch (PDOException $e) {
   echo $e->getMessage();
 }
 var_dump($database);
+
+try {
+
+  $database = new PDO("mysql:host=" . $settings['dbhost'] . ";port=" . $settings['dbport'] . ";dbname=" . $settings['dbname'], $settings['dbusername'], $settings['dbpassword']);
+} catch (PDOException $e) {
+  echo $e->getMessage();
+}
+

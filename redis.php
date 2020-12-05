a293185dd3a4cd51ec753b83ba94fe01bb600a11
Alexandre Rodriguez <alexandre@identiq.net>
2020-12-05 15:50:23 +0100

    test redis

---
+++ redis.php
@@ -0,0 +1,12 @@
+<?php require '_settings.php'; ?>
+<?php
+
+
+#nav
+$centers_list_d = $redis->get('centers_list_d');
+var_dump($centers_list_d);
+$centers_list = $database->prepare('SELECT * FROM am_centers WHERE online = ? AND aquavelo = ? ORDER BY city ASC');
+$centers_list->execute(array(1, 1));
+$centers_list_d = $centers_list->fetchAll(PDO::FETCH_ASSOC);
+$redis->set('centers_list_d', $centers_list_d);
+$redis->expire('centers_list_d', 5);

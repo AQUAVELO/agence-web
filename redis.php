222cd3b1cecebee1270c33dc1dd416d592450c7b
Alexandre Rodriguez <alexandre@identiq.net>
2020-12-05 17:23:12 +0100

    test

--- redis.php
+++ redis.php
@@ -1,11 +1,31 @@
-<?php require '_settings.php'; ?>
 <?php
+require 'vendor/autoload.php';
 
+use Phpfastcache\CacheManager;
+use Phpfastcache\Drivers\Redis\Config;
+try {
+  $redis = CacheManager::getInstance('redis', new Config([
+          'host' => getenv("REDIS_HOST"),
+          'port' => getenv("REDIS_PORT"),
+          'password' => getenv("REDIS_PASSWORD"),
+  ]));
+} catch (Exception $e) {
+  echo "Couldn't connected to Redis";
+  echo $e->getMessage();
+}
 
 #nav
-$centers_list_d = $redis->get('centers_list_d');
-$centers_list = $database->prepare('SELECT * FROM am_centers WHERE online = ? AND aquavelo = ? ORDER BY city ASC');
-$centers_list->execute(array(1, 1));
-$centers_list_d = $centers_list->fetchAll(PDO::FETCH_ASSOC);
-$redis->set('centers_list_d', $centers_list_d);
-$redis->expire('centers_list_d', 5);
+$centers_list_d_cache = $redis->getItem('centers_list_d');
+if (!$centers_list_d_cache->isHit()) {
+  $centers_list = $database->prepare('SELECT * FROM am_centers WHERE online = ? AND aquavelo = ? ORDER BY city ASC');
+  $centers_list->execute(array(1, 1));
+  $centers_list_d = $centers_list->fetchAll(PDO::FETCH_ASSOC);
+  $centers_list_d_cache->set($centers_list_d)->expiresAfter(getenv("REDIS_TTL"));
+  $redis->save($centers_list_d_cache);
+} else {
+  $centers_list_d = $centers_list_d_cache->get();
+}
+?>
+<?php foreach ($centers_list_d as &$row_centers_list) { ?>
+  <li><a href="/centres/<?= $row_centers_list['city']; ?>" title="Aquabiking à <?= $row_centers_list['city']; ?>"><?= $row_centers_list['city']; ?></a></li>
+<?php } ?>

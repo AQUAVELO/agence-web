46157dfa8d38b5038e53d8bf8623f00ed2543df0
Alexandre Rodriguez <alexandre@identiq.net>
2020-12-06 13:52:36 +0100

    segment

--- index.php
+++ index.php
@@ -99,6 +99,12 @@ if (isset($_GET['city'])) {
   <script src="/bootstrap/js/html5shiv.js"></script>
   <script src="/bootstrap/js/respond.min.js"></script>
   <![endif]-->
+  <script>
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.13.1";
+  analytics.load("NbkSIsYp06eaohctPba8atbY90rGaxQs");
+  analytics.page();
+  }}();
+</script>
 
 </head>
 
@@ -266,6 +272,7 @@ if (isset($_GET['city'])) {
   <!--<script src="/twitter/js/jquery.tweet.js"></script> -->
   <script src="/js/main.js"></script>
   <script>
+    /*
     (function(i, s, o, g, r, a, m) {
       i['GoogleAnalyticsObject'] = r;
       i[r] = i[r] || function() {
@@ -280,6 +287,7 @@ if (isset($_GET['city'])) {
 
     ga('create', 'UA-40045225-6', 'auto');
     ga('send', 'pageview');
+    */
 
     document.addEventListener("DOMContentLoaded", function(event) {
       document.querySelectorAll('img').forEach(function(img) {

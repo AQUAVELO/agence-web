ce803e76e61297e9837abd4d5918fa1e565ac7d1
kategriss <96909306+kategriss@users.noreply.github.com>
2022-01-06 13:10:32 +0100

    changement image/texte slider

--- _slider.php
+++ _slider.php
@@ -5,8 +5,10 @@
           <div class="inner">
             <div class="row">
               <div class="col-md-6 pull-left animated" data-fx="fadeIn" style="width: 100%; text-align: center; margin-top:0;">
-                <h3 class="pa slidertext" style="float:left; text-align:center;"><a href="/seance-decouverte/Antibes">Pratiquez 2 à 3 séances par semaine pour affiner votre silhouette<br />
-				<span style="font-size:60%; line-height:1em;"></span>
+                <h3 class="pa slidertext" style="float:left; text-align:center; text-transform: uppercase; color: #fff;text-shadow: -4px 4px 4px purple !important;"><a href="/seance-decouverte/Antibes"><span style="color:000;">"</span>Une bonne <span style="color:f0b0fb;">santé</span>,
+				<br />une belle <span style="color:f0b0fb;">silouhette !<span style="color:f0b0fb;">"</span></span><br />
+				Jusqu'à <span style="font-size: 120%; line-height: 1em; color: #000; -webkit-text-stroke: 2px #fff;">125€</span> de remise
+				<br />sur votre formule				
 				</a>
 				</h3>
                   <a href="/seance-decouverte/Antibes" id="sticker-text" style="float:left; clear: both;  background-image: url(/images/sticker1-orange.png); background-repeat: no-repeat; background-size: cover; display: inline-block;  padding: 4.5rem 5rem 2rem; text-decoration:none;">

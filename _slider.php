6eeb570c89520dd5c716f91865d4af8349f34c8c
kategriss <96909306+kategriss@users.noreply.github.com>
2022-05-30 12:43:25 +0200

    MAJ changement image/texte slider
    modification texte

--- _slider.php
+++ _slider.php
@@ -5,17 +5,17 @@
           <div class="inner">
             <div class="row">
               <div class="col-md-6 pull-left animated" data-fx="fadeIn" style="width: 100%; text-align: center; margin-top:0;">
-                <h3 class="pa slidertext" style="float:left; text-align:center; max-width:500px; color: #fff; text-shadow: -4px 4px 4px purple !important; font-size: 40px !important; line-height: 45px !important;"><a href="/seance-decouverte/Antibes"><span style="color: #1c4296;text-transform: uppercase;text-shadow: none !important;-webkit-text-stroke-color: #1c4296; font-size: 130%; line-height: 60px;">
-				Les<br /><strong style="font-size:120%; color: #e54488;">5 premières</strong>
+                <h3 class="pa slidertext" style="float:right; text-align:center; max-width:500px; color: #fff; text-shadow: -4px 4px 4px purple !important; font-size: 40px !important; line-height: 45px !important;"><a href="/seance-decouverte/Antibes"><span style="color: #1c4296;text-transform: uppercase;text-shadow: none !important;-webkit-text-stroke-color: #1c4296; font-size: 130%; line-height: 60px;">
+				Les <strong style="font-size:140%; color: #e54488; -webkit-text-stroke-color: #e54488;">5 premières</strong>
 				<br />séances sont
-				<br /><strong style="font-size:140%; color: #e54488;">gratuites</strong></span>
+				<br /><strong style="font-size:140%; color: #e54488; -webkit-text-stroke-color: #e54488;">gratuites !</strong></span>
 				
 				<!--<br /><span style="font-size: 33px; font-weight: normal; text-transform: none; text-shadow: 2px 2px 3px #000; display: block; color: #fff; -webkit-text-stroke-width: 0; margin-top: 20px;">
 				Jusqu'à <strong style="font-size:140%;">-100 €</strong>
 				<br />sur votre formule*</span>-->
 				</a>
 				</h3>
-                  <a href="/seance-decouverte/Antibes" id="sticker-text" style="float:left; clear: both;  background-image: url(/images/sticker1-orange.png); background-repeat: no-repeat; background-size: cover; display: inline-block;  padding: 4.5rem 5rem 2rem; text-decoration:none;">
+                  <a href="/seance-decouverte/Antibes" id="sticker-text" style="float:left; clear: both;  background-image: url(/images/sticker1-orange.png); background-repeat: no-repeat; background-size: cover; display: inline-block;  padding: 4.5rem 5rem 2rem; text-decoration:none; margin-top: 100px;">
                   <p style="font-weight: 300;">ESSAYEZ<br> UNE SÉANCE<br>GRATUITE<br>d'AQUAVELO</p>
 			    </a> 
               </div>

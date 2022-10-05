8b64109b44bd33de5a0f44091c2bfb82284f531f
kategriss <96909306+kategriss@users.noreply.github.com>
2022-10-05 15:09:53 +0200

    10/22 MAJ changement image/texte slider
    modification css

--- _slider.php
+++ _slider.php
@@ -1,13 +1,13 @@
   <section class="flexslider std-slider" data-height="740" data-loop="true" data-smooth="false" data-slideshow="true" data-speed="15000" data-animspeed="550" data-controls="true" data-dircontrols="true">
     <ul class="slides">
     <li data-bg="/images/content/home-v1-slider-03.jpg" style="background-position:top; min-height: 740px !important;">
-        <div class="container" style="width: 1400px !important;">
+        <div class="container">
           <div class="inner">
             <div class="row">
               <div class="col-md-6 pull-left animated" data-fx="fadeIn" style="width: 100%; text-align: center; margin-top:0;">
                 <h3 class="pa slidertext" style="text-align:center; max-width:600px; color: orange; text-shadow: -4px 4px 4px black !important; -webkit-text-stroke-width:0px;"><a href="/seance-decouverte/Antibes"><span style="-webkit-text-stroke-color: blue; font-family: 'Lucida Handwriting'; font-weight: 100; -webkit-text-stroke-width: 0px !important;">
 				L'Automne<br />sera sportif !</span>
-				<br /><strong style="color: #fff; -webkit-text-stroke-color: #fff; font-size:65px;">En octobre, vos</strong>
+				<br /><strong class="bigger" style="color: #fff; -webkit-text-stroke-color: #fff;">En octobre, vos</strong>
 				<br /><span class="bigger" style="color: orange; top: 20px; position: relative;">5</span>
 				<span class="smaller" style="color: #fff;">premières</span>
 				<span class="bigger" style="color: orange;">séances</span>

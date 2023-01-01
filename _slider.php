be4276f4a499a63ac1ff3e35df053d68ab4244ad
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-01-01 16:18:44 +0100

    Update _slider.php
    Modification texte

--- _slider.php
+++ _slider.php
@@ -11,8 +11,9 @@
 		      	   <br />
 			   <br />
 			  <br />
-			   <br /><span class="bigger" style="color: #f2e4b3; font-size: 100%; line-height: 40px !important;">Objectif 2023 :</span>
 		       	  <br />
+			   <br /><span class="bigger" style="color: #f2e4b3; font-size: 100%; line-height: 40px !important;">Objectif 2023 :</span>
+		      
 			   <br /> <span class="bigger" style="color: #f2e4b3; font-size: 135%; line-height: 40px !important;">Ma plus belle silhouette !</span>
 			   <br /><br /><span class="smaller" style="font-size: 20% !important;"></span>
 			   </a>

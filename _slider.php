0b175395085a66a526d2fdc4986b1858e2856c0e
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-01-31 17:35:19 +0100

    modification hauteur caractère

--- _slider.php
+++ _slider.php
@@ -16,7 +16,7 @@
 		      	   <br>
 			   <br /> <span class="bigger" style="color: #e51cb3; font-size: 100%; line-height: 40px !important;">à deux</span>
 			   <br/>
-		       	   <br/><span class="bigger" style="color: #e51cb3; style="font-size:50%; line-height: 40px !important; ">c'est encore mieux !</span>
+		       	   <br/><span class="bigger" style="color: #e51cb3; font-size:50%; line-height: 40px !important; ">c'est encore mieux !</span>
 			   </a>
 				</h3>
 				

bf3436f67d17547f380cc3e4ecee4ce42f6fc09e
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-01-31 17:37:52 +0100

    modification texte

--- _slider.php
+++ _slider.php
@@ -14,9 +14,9 @@
 		       	  <br />
 			   <br /><span class="bigger" style="color: #e51cb3; font-size: 100%; line-height: 40px !important;"></span>
 		      	   <br>
-			   <br /> <span class="bigger" style="color: #e51cb3; font-size: 100%; line-height: 40px !important;">à deux</span>
+			   <br /> <span class="bigger" style="color: #e51cb3; font-size: 100%; line-height: 40px !important;">À deux</span>
 			   <br/>
-		       	   <br/><span class="bigger" style="color: #e51cb3; font-size:50%; line-height: 40px !important; ">c'est encore mieux !</span>
+		       	   <br/><span class="bigger" style="color: #e51cb3; font-size:80%; line-height: 40px !important; ">c'est encore mieux !</span>
 			   </a>
 				</h3>
 				

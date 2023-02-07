c8229868a6b81ddeaa4e229133c80ae0efaf2c88
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-02-07 16:05:36 +0100

    modification seance essai

--- _slider.php
+++ _slider.php
@@ -21,7 +21,7 @@
 				</h3>
 				
 				
-                  <a href="/seance-decouverte/Antibes" id="sticker-text" style="float:left; clear: both;  background-image: url(/images/sticker1-orange.png); background-repeat: no-repeat; background-size: cover; display: inline-block;  padding: 4.5rem 5rem 2rem; text-decoration:none; margin-top: 100px;">
+                  <a href="/seance-decouverte/Cannes" id="sticker-text" style="float:left; clear: both;  background-image: url(/images/sticker1-orange.png); background-repeat: no-repeat; background-size: cover; display: inline-block;  padding: 4.5rem 5rem 2rem; text-decoration:none; margin-top: 100px;">
                   <p style="font-weight: 300;">ESSAYEZ<br> UNE SÉANCE<br>GRATUITE<br>d'AQUAVELO</p>
 			    </a> 
               </div>

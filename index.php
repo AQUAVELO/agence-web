700cdbf3c3636562f5570a1b6c4e9391af593880
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-02-13 21:42:40 +0100

    ajout google tag 104

--- index.php
+++ index.php
@@ -126,16 +126,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   }}();
 </script>
   
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58JPX2R"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+  
   
 
 </head>
 
 <body class="withAnimation">
   
-  <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58JPX2R"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
   
   <div id="boxedWrapper">
 

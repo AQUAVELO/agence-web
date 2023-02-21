d9116ad819b290baa14ff2ba123abe79e106ca54
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-02-21 22:14:41 +0100

    video modifiée en 80 %

--- _aquabiking.php
+++ _aquabiking.php
@@ -14,7 +14,7 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6">
-		<video controls="controls" width="580" height="360" name="Video AQUAVELO de Antibes">
+		<video controls="controls" width="80%"  name="Video AQUAVELO de Antibes">
 			<source src="https://www.aquavelo.com/Video-aquavelo-de-Antibes.mov">
 		</video>
 	  

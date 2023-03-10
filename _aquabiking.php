ec2917a177cab214a644622ef11d0ee4cc4c3a74
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-03-10 18:57:21 +0100

    suppression modale

--- _aquabiking.php
+++ _aquabiking.php
@@ -39,58 +39,7 @@ Ces messieurs ne seront pas en reste! La résistance de l’eau agit sur toute l
   </div>
 	
 </section>
-
-
-	<body>
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-        LES RÉSULTATS MINCEURS<br>Cliquez ici
-    </button>
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Contact</title>
-</head>
-    <!-- Modal -->
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Les résultats sur la silhouette</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th colspan="2">Voici quelques résultats obtenus avec une pratique réguliére de
-                                    l'Aquavelo</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><img src="resultats/CHRISTINE.jpg" alt="Christine a minci" width=60% /></td>
-                                <td><img src="resultats/MEDE.jpg" alt="Christine a minci" width=60% /></td>
-                            </tr>
-                            <tr>
-                                <td><img src="resultats/SOPHIE.jpg" alt="Christine a minci" width=60% /></td>
-                                <td><img src="resultats/AURELIE.jpg" alt="Christine a minci" width=60% /></td>
-                            </tr>
-                            <tr>
-                                <td><img src="resultats/SANDRA.jpg" alt="Christine a minci" width=60% /></td>
-                                <td><img src="resultats/PATY.jpg" alt="Christine a minci" width=60% /></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-                    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-                    crossorigin="anonymous"></script>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
- </body>	
+	
 
 
 

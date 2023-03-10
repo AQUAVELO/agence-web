1c93ac1c6b831b5eaf1f30dd27992be64446ba20
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-03-10 14:19:37 +0100

    ajout Modal

--- _page.php
+++ _page.php
@@ -52,6 +52,9 @@ src="https://www.facebook.com/tr?id=259009481449831&ev=PageView
 
           <?php } ?>
 	
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+	
 	
 </header>
 <section class="content-area bg1">
@@ -96,7 +99,46 @@ src="https://www.facebook.com/tr?id=259009481449831&ev=PageView
 		   <dd>
 		
           <dt>Résultats Minceurs Rapides</dt>
-          <dd>  </dd>
+          <dd><!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        LES RÉSULTATS MINCEURS<br>Cliquez ici
+    </button>  </dd>
+		   
+		    
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Les résultats sur la silhouette</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th colspan="2">Voici quelques résultats obtenus avec une pratique réguliére de
+                                    l'Aquavelo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><img src="resultats/CHRISTINE.jpg" alt="Christine a minci" width=60% /></td>
+                                <td><img src="resultats/MEDE.jpg" alt="Christine a minci" width=60% /></td>
+                            </tr>
+                            <tr>
+                                <td><img src="resultats/SOPHIE.jpg" alt="Christine a minci" width=60% /></td>
+                                <td><img src="resultats/AURELIE.jpg" alt="Christine a minci" width=60% /></td>
+                            </tr>
+                            <tr>
+                                <td><img src="resultats/SANDRA.jpg" alt="Christine a minci" width=60% /></td>
+                                <td><img src="resultats/PATY.jpg" alt="Christine a minci" width=60% /></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
          
 
 	   </dd>
@@ -127,6 +169,15 @@ src="https://www.facebook.com/tr?id=259009481449831&ev=PageView
       </div>
     </div>
   </div>
+
+		   
+
+		   
+   
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+                    crossorigin="anonymous"></script>
+
 </section>
 <!--
 <section class="content-area bg2">

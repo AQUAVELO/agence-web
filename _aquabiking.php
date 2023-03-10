a9fe9b51a4ce9cde878eda265c94a5b284dff7cc
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-03-10 18:31:24 +0100

    ligne 11

--- _aquabiking.php
+++ _aquabiking.php
@@ -1,7 +1,4 @@
-<head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
- 	
-</head>	
+
 <header class="main-header clearfix">
   <div class="container">
     <h1 class="page-title pull-left">Aquabiking</h1>
@@ -11,6 +8,8 @@
       <li class="active">Aquabiking</li>
     </ol>
   </div>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
 </header>
 
 <section class="content-area bg1">

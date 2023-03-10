fa2afae63de70984af6fbca28738b99963cdc775
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-03-10 18:33:40 +0100

    sup link

--- _aquabiking.php
+++ _aquabiking.php
@@ -8,8 +8,7 @@
       <li class="active">Aquabiking</li>
     </ol>
   </div>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
+	
 </header>
 
 <section class="content-area bg1">

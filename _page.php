4911a53026ffbe7ce75d9ac02d513a67da9a7b08
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-03-10 19:08:39 +0100

    modi modale

--- _page.php
+++ _page.php
@@ -126,17 +126,22 @@ src="https://www.facebook.com/tr?id=259009481449831&ev=PageView
         </dl>
 		   
 
+
+		   <!doctype html>
+<html>
+<head>
+
+<meta charset="UTF-8">
+		   <title>Contact</title>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</head>
 	<body>
     <!-- Button trigger modal -->
     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
         LES RÉSULTATS MINCEURS<br>Cliquez ici
     </button>
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Contact</title>
-</head>
+
     <!-- Modal -->
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">

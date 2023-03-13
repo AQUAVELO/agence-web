e6479673507593a3670d6b3ce1e2a8c532805c82
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-03-13 23:38:03 +0100

    sup resultat

--- _page.php
+++ _page.php
@@ -98,7 +98,7 @@ src="https://www.facebook.com/tr?id=259009481449831&ev=PageView
 	  <dd>
 		
           <dt>Résultats Minceurs Rapides</dt>
-          <dd> <A href="#" onClick="window.open('/resultat.html','Résultats Minceurs','toolbar=0, location=0, directories=0, status=0, scrollbars=0, resizable=0, copyhistory=0, menuBar=0, width=700, height=700');return(false)">Voir</A> </dd>
+          <dd>  </dd>
 		   
 		      		             
 

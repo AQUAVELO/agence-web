efd35689a8ce35c3b60800b7690ab732ce16c549
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-03-23 18:40:51 +0100

    window open en JS

--- _page.php
+++ _page.php
@@ -34,6 +34,12 @@ s.parentNode.insertBefore(t,s)}(window,document,'script',
  fbq('init', '259009481449831'); 
 
 fbq('track', 'PageView');
+	
+var windowReference = window.open();
+
+myService.getUrl().then(function(url) {
+     windowReference.location = url;
+});
 
 </script>
 

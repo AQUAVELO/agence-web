0d6b04395ccd70058bf8bae2b3e012f14c52b7ff
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-10-12 16:40:58 +0200

    Update _page.php ajout page 69

--- _page.php
+++ _page.php
@@ -66,6 +66,7 @@ src="https://www.facebook.com/tr?id=259009481449831&ev=PageView
         <div class="row" style="margin-top:30px;">
           <div class="col-md-6"> <img src="/cloud/thumbnail/center_<?= $row_center['id']; ?>/2.jpg" alt=" "> </div>
           <div class="col-md-6"> <img src="/cloud/thumbnail/center_<?= $row_center['id']; ?>/3.jpg" alt=" "> </div>
+	<div class="col-md-6"> <img src="/cloud/thumbnail/center_<?= $row_center['id']; ?>/3.jpg" alt=" "> </div>
         </div>
       </div>
 	   

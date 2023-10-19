60b041234e703c40b0884dabb649c5f70fc7f723
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2023-10-19 17:41:22 +0200

    Update _page.php aligner

--- _page.php
+++ _page.php
@@ -1,6 +1,6 @@
 <header class="main-header clearfix">
   <div class="container">
-   <h1 class="page-title pull-left">L'aquabiking est efficace pour l'affinement de la silhouette, la tonification et le bien être</h1>
+   <h1 class="page-title pull-left" align="center">L'aquabiking est efficace pour l'affinement de la silhouette, la tonification et le bien-être.</h1>
     <ol class="breadcrumb pull-right">
       <li><a href="./">Accueil</a></li>
       <li><a href="/centres">Centres</a></li>

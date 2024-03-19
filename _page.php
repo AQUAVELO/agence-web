4131481fbb16d811f508d1affb87fd7747bf2b76
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2024-03-19 21:55:04 +0100

    Update _page.php 81 108

--- _page.php
+++ _page.php
@@ -81,6 +81,33 @@ src="https://www.facebook.com/tr?id=259009481449831&ev=PageView
       <div class="col-md-6">
          <a href="/seance-decouverte/<?= $row_center['city']; ?>" class="btn btn-default" style="margin-top: 30px; color: #0f38be;">J'essaie un cours Gratuit de 45 mn</a>
 
+	<div class="col-md-6">
+        <form role="form" class="contact-form" method="POST" action="?">
+        
+          <div class="form-group">
+            <label for="nom">Nom et prénom</label>
+            <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom et prénom">
+          </div>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <label for="phone">Téléphone</label>
+            <input type="phone" class="form-control" id="phone" name="phone" placeholder="Téléphone">
+          </div>
+          <div class="form-group">
+            <label for="nom">Jour et heure du rdv souhaité (facultatif)</label>
+            <input type="text" class="form-control" id="rdv" name="rdv" placeholder="Jour et heure du rdv souhaité (facultatif)">
+          </div>
+          <input type="hidden" name="reason" id="reason">
+          <input type="hidden" name="segment" id="segment">
+          <button type="submit" class="btn btn-default">Recevoir mon bon par email</button>
+        </form>
+      </div>
+    </div>
+
+			
          <dl style="margin-top:30px;">
           <dt>Adresse </dt>
           <dd>

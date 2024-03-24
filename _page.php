c1f63a4ca5b3a3a218d077b363258bf2626063ea
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2024-03-24 20:14:42 +0100

    Update _page.php modif ligne 84

--- _page.php
+++ _page.php
@@ -81,7 +81,7 @@ src="https://www.facebook.com/tr?id=259009481449831&ev=PageView
 
 
 			
-        <h2> Essayez une séance gratuite de 45 mn </h2>
+        <h2 class="form-group"> Essayez une séance gratuite de 45 mn </h2>
 			<div class="col-md-6">
         <form role="form" class="contact-form" method="POST" action="?">
 

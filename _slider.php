7827f1155e83086249998aedb33d0f903977d471
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2024-03-29 17:52:44 +0100

    Update _slider.php

--- _slider.php
+++ _slider.php
@@ -5,37 +5,7 @@
         <div class="inner">
           <div class="row">
             <div class="col-md-6 pull-left animated" data-fx="fadeIn" style="width: 100%; text-align: center; margin-top:0;">
-              <!-- Formulaire -->
-              <h2 class="form-group" style="text-align: center;">Essayez une séance gratuite de 45 mn</h2>
-              <div class="col-md-6">
-                <form role="form" class="contact-form" method="POST" action="_page.php">
-                  <div class="form-group">
-                    <label for="center">Dans quel centre souhaitez-vous effectuer votre séance ?</label>
-                    <select class="form-control" id="center" name="center">
-                      <?php foreach ($centers_list_d as &$free_d) { ?>
-                        <option <?php if (isset($_GET['city']) &&  $_GET['city'] == $free_d['city']) echo 'selected'; ?> value="<?= $free_d['id'] ?>"><?= $free_d['city'] ?></option>
-                      <?php } ?>
-                    </select>
-                  </div>
-
-                  <div class="form-group">
-                    <label for="nom">Nom et prénom</label>
-                    <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom et prénom">
-                  </div>
-                  <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-                  </div>
-                  <div class="form-group">
-                    <label for="phone">Téléphone</label>
-                    <input type="phone" class="form-control" id="phone" name="phone" placeholder="Téléphone">
-                  </div>
-                  <input type="hidden" name="reason" id="reason">
-                  <input type="hidden" name="segment" id="segment">
-                  <button type="submit" class="btn btn-default">Recevoir mon bon par email</button>
-                </form>
-              </div>
-              <!-- Fin du formulaire -->
+              
 
               <h3 class="pa slidertext" style="text-align:center; color: #fff; line-height: 40px !important; text-shadow: -4px 4px 4px black !important; -webkit-text-stroke-width:0px; max-width: 625px;">
                 <a href="/seance-decouverte/Antibes">
@@ -86,4 +56,36 @@
     </li>
   </ul>
 </section>
+
+<!-- Formulaire -->
+              <h2 class="form-group" style="text-align: center;">Essayez une séance gratuite de 45 mn</h2>
+              <div class="col-md-6">
+                <form role="form" class="contact-form" method="POST" action="_page.php">
+                  <div class="form-group">
+                    <label for="center">Dans quel centre souhaitez-vous effectuer votre séance ?</label>
+                    <select class="form-control" id="center" name="center">
+                      <?php foreach ($centers_list_d as &$free_d) { ?>
+                        <option <?php if (isset($_GET['city']) &&  $_GET['city'] == $free_d['city']) echo 'selected'; ?> value="<?= $free_d['id'] ?>"><?= $free_d['city'] ?></option>
+                      <?php } ?>
+                    </select>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="nom">Nom et prénom</label>
+                    <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom et prénom">
+                  </div>
+                  <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                  </div>
+                  <div class="form-group">
+                    <label for="phone">Téléphone</label>
+                    <input type="phone" class="form-control" id="phone" name="phone" placeholder="Téléphone">
+                  </div>
+                  <input type="hidden" name="reason" id="reason">
+                  <input type="hidden" name="segment" id="segment">
+                  <button type="submit" class="btn btn-default">Recevoir mon bon par email</button>
+                </form>
+              </div>
+              <!-- Fin du formulaire -->
 

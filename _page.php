79fbb99f47f1648ccdee69e993b5c5e405830ed5
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2024-04-10 17:04:10 +0200

    Update _page.php

--- _page.php
+++ _page.php
@@ -65,13 +65,43 @@ src="https://www.facebook.com/tr?id=259009481449831&ev=PageView
 <section class="content-area bg1">
   <div class="container">
     <div class="row">
+
 	    
-      <div class="col-md-6"> <img src="/cloud/thumbnail/center_<?= $row_center['id']; ?>/1.jpg" alt="Aquavélo">
-        <div class="row" style="margin-top:30px;">
-          <div class="col-md-6"> <img src="/cloud/thumbnail/center_<?= $row_center['id']; ?>/2.jpg" alt="Aquavélo"> </div>   
-          <div class="col-md-6"> <img src="/cloud/thumbnail/center_<?= $row_center['id']; ?>/3.jpg" alt="Aquavélo"> </div>
+	    
+ <div id="imageCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#imageCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#imageCarousel" data-slide-to="1"></li>
+        <li data-target="#imageCarousel" data-slide-to="2"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="/cloud/thumbnail/center_<?= $row_center['id']; ?>/1.jpg" alt="Image 1">
         </div>
-	
+        <div class="item">
+          <img src="/cloud/thumbnail/center_<?= $row_center['id']; ?>/2.jpg" alt="Image 2">
+        </div>
+        <div class="item">
+          <img src="/cloud/thumbnail/center_<?= $row_center['id']; ?>/3.jpg" alt="Image 3">
+        </div>
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#imageCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#imageCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+    </div>
+  </div>
+
+
+
+	      
             <?php if($row_center['id'] == 305)  { ?>
 		<div class="animated activate fadeInLeft"><a href="https://www.aquavelo.com/seance-decouverte/Cannes"><img src="/images/promoJan24.webp" alt="Promotion Aquavelo"> </div>;
 		 <?php } elseif ($row_center['id'] == 253) { ?>

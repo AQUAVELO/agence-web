9f3110221749b8e689f4efe6bacd0391d9ff85f6
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2024-06-07 22:22:03 +0200

    Update envoi.php

--- envoi.php
+++ envoi.php
@@ -83,6 +83,7 @@ function sendThankYouEmail($toEmail, $toName, $settings) {
     
         <p>Je vous remercie de votre confiance. À présent vous devez cliquer sur <a href="https://www.aquavelo.com/_analyse.php" target="_blank">ce lien</a> pour rentrer vos coordonnées et mensurations.</p>
         <p class="signature">Cordialement, Claude</p>
+        <p>AQUAVELO</p>
     </div>
 </body>
 </html>

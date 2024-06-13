2a4163907c9b7ea0d69c34c98e342480b9c3e485
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2024-06-13 14:39:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -238,8 +238,9 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
                  <li <?php if ($p == 'Conseils minceurs') echo ' class="active"'; ?> class="dropdown<?php if ($p == 'Conseils minceurs') echo ' active'; ?>"> <a href="/Conseils minceurs" class="dropdown-toggle" data-toggle="dropdown" data-target="#">Conseils minceurs</a>
                  <ul class="dropdown-menu">
-                  <li><a href="/_analyse.php">Analyse de vos mensurations</a></li>
                   <li><a href="https://www.dietsuccess.fr">Conseils minceurs</a></li>
+                  <li><a href="/_analyse.php">Analyse de vos mensurations</a></li>
+                 
                 </ul>
             
               <li <?php if ($p == 'contact') echo ' class="active"'; ?> class="dropdown<?php if ($p == 'contact') echo ' active'; ?>"> <a href="/contact" class="dropdown-toggle" data-toggle="dropdown" data-target="#">Contact</a>

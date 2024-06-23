679ed9877d5e8fc68b82906a89636d71c17b8d5b
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2024-06-23 18:16:21 +0200

    Update _aquabiking.php

--- _aquabiking.php
+++ _aquabiking.php
@@ -257,7 +257,7 @@
     </header>
     <div class="row">
       <div class="col-md-12">
-        <iframe src="http://www.partenaires.aquavelocannes.fr/affichagelocal.php" width="100%" height="600px" frameborder="0" allowfullscreen></iframe>
+        <iframe src="https://www.partenaires.aquavelocannes.fr/affichagelocal.php" width="100%" height="600px" frameborder="0" allowfullscreen></iframe>
       </div>
     </div>
   </div>

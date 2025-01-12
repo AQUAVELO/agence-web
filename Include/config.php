c644872576e8cc90dae7b8ca596b881e759d5e39
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-01-12 15:54:04 +0100

    Create config.php

---
+++ Include/config.php
@@ -0,0 +1,3 @@
+<?php
+define('API_KEY', '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');
+?>

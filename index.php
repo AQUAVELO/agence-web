09a1955a1eb0d07d5be396b2bbff554d84dad469
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-01-18 07:29:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -34,11 +34,10 @@ $menu_query->bindParam(':jour_du_mois', $jour_du_mois, PDO::PARAM_INT);
 $menu_query->execute();
 $menu_data = $menu_query->fetch(PDO::FETCH_ASSOC);
 
-// requete pour récuperer les news 
-$news_query = $database->prepare('SELECT news, photo, titre FROM article ');
-$news_query->bindParam(':condition', $valeur_condition, PDO::PARAM_STR); // ou PDO::PARAM_INT si la condition est un entier
+// Requête pour récupérer tous les articles
+$news_query = $database->prepare('SELECT news, photo, titre FROM article');
 $news_query->execute();
-$news_data = $news_query->fetch(PDO::FETCH_ASSOC);
+$news_data = $news_query->fetchAll(PDO::FETCH_ASSOC); // Récupère toutes les lignes
 
 
 

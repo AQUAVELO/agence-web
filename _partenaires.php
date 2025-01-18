9ce86aa88aa58aeb942b2aff923e5aed4cdca841
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-01-18 07:35:40 +0100

    Update _partenaires.php

--- _partenaires.php
+++ _partenaires.php
@@ -1,11 +1,5 @@
 <?php
-// Connexion à la base de données
-try {
-    $database = new PDO('mysql:host=localhost;dbname=nom_de_votre_base', 'utilisateur', 'mot_de_passe');
-    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
-}
+
 
 // Requête pour récupérer tous les articles
 $news_query = $database->prepare('SELECT news, photo, titre FROM article');

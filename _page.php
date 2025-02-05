89859b1d3cf2611250bc2422adbb94ded4540db7
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-02-05 09:14:38 +0100

    Update _page.php

--- _page.php
+++ _page.php
@@ -156,7 +156,7 @@
         <!-- Découvrez la vie de votre centre -->
 
     	
-    <?php if ($row_center['id'] == 253 && !empty($row_center['book_link'])) { ?>
+  
     	<dt>Agenda pour les adhérents</dt>
     	<dd>
      	 <a href="https://<?= htmlspecialchars($row_center['book_link'], ENT_QUOTES, 'UTF-8'); ?>" 
@@ -165,7 +165,7 @@
         	 target="_blank" 
         	 class="btn btn-default">Réserver en ligne</a>
    	 </dd>
-     <?php } ?>
+   
 
 	      
 

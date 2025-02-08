3f27919ee70f7e9b73a47051e97cfe99590191cc
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-02-08 22:44:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -467,6 +467,19 @@ window.axeptioSettings = {
 
     })
   </script>
+  
+  <script>
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker enregistré avec succès:', registration);
+    })
+    .catch(function(error) {
+      console.log('Échec de l’enregistrement du Service Worker:', error);
+    });
+  }
+</script>
+
 </body>
 
 </html>

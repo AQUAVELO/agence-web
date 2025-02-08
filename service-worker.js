7dac493c0f6dc42676e894a7d2a19d19ae9f0c2d
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-02-08 22:31:30 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,16 @@
 self.addEventListener('install', function(event) {
   console.log('Service Worker installé.');
+
   event.waitUntil(
     caches.open('aquavelo-cache').then(function(cache) {
       return cache.addAll([
         '/',
         '/centres/Cannes',
         '/centres/Antibes',
-        '/images/Aquavelo_Icon.png'
+        '/conseilminceur',                    // ✅ Ajout de Conseil Minceur
+        '/images/Aquavelo_Icon.png',
+        '/images/Aquavelo_Icon_192-M.png',    // ✅ Nouvelle icône
+        '/images/Aquavelo_Icon_M.png'         // ✅ Nouvelle icône
       ]);
     })
   );

fa312b1650108fd8569db65cd3c9af7802058bb9
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-03-30 18:43:16 +0200

    Update config.php

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ $stripePublicKey = getenv('STRIPE_PUBLISHABLE_KEY') ?: 'pk_test_51R0razK3GWmEE8I
 stripe\Stripe::setApiKey($stripeSecretKey);
 
 // Vérification pour être certain que l'environnement est bien configuré.
-if ($stripeSecretKey == 'sk_test_xxxxxxxxxxxxx' || $stripePublicKey == 'pk_test_xxxxxxxxxxxxx') {
+if ($stripeSecretKey == 'sk_test_51R0razK3GWmEE8Ic7OHpRMz8BcnfnuYSHmixqzKPD8Ww3UVrY5Fsti9B3Hiq8canrQ36M6lYpOSDg7uZcocLfikg00ipggxD7x' || $stripePublicKey == 'pk_test_51R0razK3GWmEE8IcMClhIck5shXPh8iGQ41LwFbec7CTnfK3pKGbUA5FpW9Zw4Bm3Ho2GckSpEUMl7FKyggvD07S004FHPm73I') {
     error_log("⚠️  Les clés Stripe ne sont pas correctement configurées. L'application ne fonctionnera pas !!!");
 }
 ?>

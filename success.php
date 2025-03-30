ec56a5f05f9cc09da5d6c652ffc30aef7b057591
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-03-30 17:25:27 +0200

    Create success.php

---
+++ success.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Merci - Votre commande a été effectuée</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<!-- Ton contenu de page de succès -->
+Succès pour le paiement
+</body>
+</html>

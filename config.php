9fdf2eb7ed32a81baa2918cc51c7b2eabb153d94
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-03-30 18:42:30 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 
 
 // Tes clés d'API Stripe (à remplacer par tes vraies clés)
-$stripeSecretKey = getenv('STRIPE_SECRET_KEY') ?: 'sk_test_xxxxxxxxxxxxx'; // Utilise la variable d'env si définie
-$stripePublicKey = getenv('STRIPE_PUBLISHABLE_KEY') ?: 'pk_test_xxxxxxxxxxxxx';  // Utilise la variable d'env si définie
+$stripeSecretKey = getenv('STRIPE_SECRET_KEY') ?: 'sk_test_51R0razK3GWmEE8Ic7OHpRMz8BcnfnuYSHmixqzKPD8Ww3UVrY5Fsti9B3Hiq8canrQ36M6lYpOSDg7uZcocLfikg00ipggxD7x'; // Utilise la variable d'env si définie
+$stripePublicKey = getenv('STRIPE_PUBLISHABLE_KEY') ?: 'pk_test_51R0razK3GWmEE8IcMClhIck5shXPh8iGQ41LwFbec7CTnfK3pKGbUA5FpW9Zw4Bm3Ho2GckSpEUMl7FKyggvD07S004FHPm73I';  // Utilise la variable d'env si définie
 
 //Définition de la clé secrète
 stripe\Stripe::setApiKey($stripeSecretKey);

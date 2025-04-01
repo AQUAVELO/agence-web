db651a7c42df859e7a10373c91775ded3ab74f30
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-04-01 09:30:16 +0200

    Update vente_formule.php

--- vente_formule.php
+++ vente_formule.php
@@ -1,22 +1,22 @@
 <?php
 // Informations Monetico (à remplacer par vos informations)
-$monetico_tpe = "VOTRE_TPE"; // Numéro TPE
-$monetico_cle = "VOTRE_CLE"; // Clé de sécurité
-$monetico_devise = "EUR"; // Devise
-$monetico_langue = "FR"; // Langue
-$monetico_url_retour = "https://www.aquavelo.com/confirmation.php"; // URL de retour après paiement
-$monetico_url_notify = "https://www.aquavelo.com/notification.php"; // URL de notification (IPN)
-$monetico_mail_surveillance = "votre@email.com"; // Email de surveillance
-$monetico_version = "3.0"; // Version du protocole
-$monetico_societe = "VOTRE_SOCIETE"; // Nom de votre société
-$monetico_url_home = "http://votresite.com"; // URL de votre site
+$monetico_tpe = "VOTRE_TPE";
+$monetico_cle = "VOTRE_CLE";
+$monetico_devise = "EUR";
+$monetico_langue = "FR";
+$monetico_url_retour = "https://www.aquavelo.com/confirmation.php";
+$monetico_url_notify = "https://www.aquavelo.com/notification.php";
+$monetico_mail_surveillance = "votre@email.com";
+$monetico_version = "3.0";
+$monetico_societe = "AQUACANNES";
+$monetico_url_home = "https://www.aquavelo.com/vente_formule.php";
 
 // Informations de la commande
-$montant = "99.00"; // Montant de la commande
-$reference = uniqid(); // Référence unique de la commande
+$montant = "99.00";
+$reference = uniqid();
 
 // Calcul de la signature
-$texte_libre = ""; // Texte libre (peut être vide)
+$texte_libre = "";
 $date = gmdate("YmdHis");
 $concat = $monetico_tpe . "*" . $date . "*" . $montant . "*" . $monetico_devise . "*" . $reference . "*" . $texte_libre . "*" . $monetico_cle;
 $signature = hash("sha256", $concat);
@@ -28,30 +28,54 @@ $url_monetico = "https://p.monetico-services.com/paiement.cgi";
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Achat Formule</title>
+    <title>Achat Séance Cryolipolyse - Aquavelo Cannes</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            text-align: center;
+        }
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+        .container {
+            width: 80%;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 <body>
-    <h1>Formule à 99 €</h1>
-    <p>Cliquez sur le bouton ci-dessous pour payer votre formule.</p>
+    <div class="container">
+        <h1>Achat d'une séance de Cryolipolyse - Aquavelo Cannes</h1>
+        <img src="images/cryo.jpg" alt="Séance Cryolipolyse Aquavelo Cannes">
+        <p>
+            Profitez de notre offre exceptionnelle : une séance de Cryolipolyse d'une heure à seulement 99 € !
+            Ciblez la zone de votre choix (ventre, cuisses, fessier, etc.) et définissez-la avec notre praticienne expérimentée.
+            Offre valable jusqu'au 10/04/2025.
+        </p>
+        <p>
+            Rendez-vous chez Aquavelo : 60 avenue du Docteur Raymond Picaud, Cannes.
+        </p>
 
-    <form action="<?php echo $url_monetico; ?>" method="post">
-        <input type="hidden" name="version" value="<?php echo $monetico_version; ?>">
-        <input type="hidden" name="TPE" value="<?php echo $monetico_tpe; ?>">
-        <input type="hidden" name="date" value="<?php echo $date; ?>">
-        <input type="hidden" name="montant" value="<?php echo $montant; ?>">
-        <input type="hidden" name="devise" value="<?php echo $monetico_devise; ?>">
-        <input type="hidden" name="reference" value="<?php echo $reference; ?>">
-        <input type="hidden" name="texte-libre" value="<?php echo $texte_libre; ?>">
-        <input type="hidden" name="MAC" value="<?php echo $signature; ?>">
-        <input type="hidden" name="url_retour" value="<?php echo $monetico_url_retour; ?>">
-        <input type="hidden" name="url_retour_ok" value="<?php echo $monetico_url_retour; ?>">
-        <input type="hidden" name="url_retour_err" value="<?php echo $monetico_url_retour; ?>">
-        <input type="hidden" name="lgue" value="<?php echo $monetico_langue; ?>">
-        <input type="hidden" name="societe" value="<?php echo $monetico_societe; ?>">
-        <input type="hidden" name="mail" value="<?php echo $monetico_mail_surveillance; ?>">
-        <input type="hidden" name="url_ipn" value="<?php echo $monetico_url_notify; ?>">
-        <input type="hidden" name="url_boutique" value="<?php echo $monetico_url_home; ?>">
-        <input type="submit" value="Payer 99 €">
-    </form>
+        <form action="<?php echo $url_monetico; ?>" method="post">
+            <input type="hidden" name="version" value="<?php echo $monetico_version; ?>">
+            <input type="hidden" name="TPE" value="<?php echo $monetico_tpe; ?>">
+            <input type="hidden" name="date" value="<?php echo $date; ?>">
+            <input type="hidden" name="montant" value="<?php echo $montant; ?>">
+            <input type="hidden" name="devise" value="<?php echo $monetico_devise; ?>">
+            <input type="hidden" name="reference" value="<?php echo $reference; ?>">
+            <input type="hidden" name="texte-libre" value="<?php echo $texte_libre; ?>">
+            <input type="hidden" name="MAC" value="<?php echo $signature; ?>">
+            <input type="hidden" name="url_retour" value="<?php echo $monetico_url_retour; ?>">
+            <input type="hidden" name="url_retour_ok" value="<?php echo $monetico_url_retour; ?>">
+            <input type="hidden" name="url_retour_err" value="<?php echo $monetico_url_retour; ?>">
+            <input type="hidden" name="lgue" value="<?php echo $monetico_langue; ?>">
+            <input type="hidden" name="societe" value="<?php echo $monetico_societe; ?>">
+            <input type="hidden" name="mail" value="<?php echo $monetico_mail_surveillance; ?>">
+            <input type="hidden" name="url_ipn" value="<?php echo $monetico_url_notify; ?>">
+            <input type="hidden" name="url_boutique" value="<?php echo $monetico_url_home; ?>">
+            <input type="submit" value="Payer 99 €">
+        </form>
+    </div>
 </body>
 </html>

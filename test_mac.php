77b2656f2e95eca8815606519e87bceed1cf2c13
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-04-13 04:33:53 +0200

    Update test_mac.php

--- test_mac.php
+++ test_mac.php
@@ -1,9 +1,6 @@
-<?php
-$key_hex = 'AB477436DAE9200BF71E755208720A3CD52805';
-
-$chaine = "TPE=6684349*date=13/04/2025:02:58:00*lgue=FR*mail=jacquesverdier4@gmail.com*montant=000000099.00EUR*reference=CMD20250413025800321*societe=ALESIAMINCEUR*texte-libre=Séance d'aquavelo de 45 minutes*url_retour_err=https://www.aquavelo.com/annulation.php*url_retour_ok=https://www.aquavelo.com/confirmation.php*version=3.0";
+$chaine = "TPE=6684349*date=13/04/2025:04:32:17*lgue=FR*mail=aquaveloeurope@gmail.com*montant=000000099.00EUR*reference=CMD20250413043217973*societe=ALESIAMINCEUR*texte-libre=Séance d'aquavelo de 45 minutes*url_retour_err=https://www.aquavelo.com/annulation.php*url_retour_ok=https://www.aquavelo.com/confirmation.php*version=3.0";
+$key_hex = 'AB477436DAE9200BF71E755208720A3CD5280594';
 
 $mac = strtoupper(hash_hmac('sha1', $chaine, pack('H*', $key_hex)));
+echo $mac;
 
-echo "Chaîne : $chaine\n";
-echo "MAC    : $mac\n";

7c35b7457ba19392bba37ee276629f141865a2a8
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-04-14 04:52:53 +0200

    Update annulation.php

--- annulation.php
+++ annulation.php
@@ -45,7 +45,7 @@
         <p>Votre transaction n’a pas pu être finalisée.<br>
         Les informations transmises sont incomplètes ou erronées.</p>
         <p>Si besoin, vous pouvez recommencer votre réservation.</p>
-        <a href="/formule_vente.php">Retour à l'achat de la séance de Cryo</a>
+        <a href="/vente_formule.php">Retour à l'achat de la séance de Cryo</a>
     </div>
 </body>
 </html>

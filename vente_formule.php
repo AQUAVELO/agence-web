00a955acb6119eff16b851f1401c299cd4b64a6f
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-04-16 02:39:46 +0200

    Update vente_formule.php

--- vente_formule.php
+++ vente_formule.php
@@ -212,7 +212,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <h2>Découvrez l'amincissement par cryolipolyse à 99€</h2>
 
         <div class="image-section">
-            <img src="cryolipolyse.jpg" alt="Séance de cryolipolyse">
+            <img src="images/cryolipolyse.jpg" alt="Séance de cryolipolyse">
         </div>
 
         <h2>Qu’est-ce que la Cryolipolyse ?</h2>

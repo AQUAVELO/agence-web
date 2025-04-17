f5d7c78ffb04fd1511cfaa138e52349eaefc7282
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-04-17 02:21:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ if ($page == 'concept') $title = 'Concept Aquavelo';
 if ($page == 'free') $title = 'Séance découverte';
 if ($page == 'partenaires') $title = 'Partenaires';
 if ($page == 'conseilminceur') $title = 'Minceur';
+if ($page == 'vente_formule') $title = 'vente_formule';
 if ($page == 'natation') $title = 'Natation | Aquavelo';
 if ($page == 'inscription_client') $title = 'Inscription Client | Aquavelo';
 if ($page == 'inscription_nageur') $title = 'Inscription Maître-Nageur | Aquavelo';

048b89318ed768e1ad11e7d987a7c65e955847e9
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-05-13 14:55:30 +0200

    Update _vente_prix.php

--- _vente_prix.php
+++ _vente_prix.php
@@ -2,7 +2,7 @@
 // Configuration Monetico
 define('MONETICO_TPE', '6684349');
 define('MONETICO_KEY', 'AB477436DAE9200BF71E755208720A3CD5280594');
-define('MONETICO_COMPANY', 'AQUACANNES');
+define('MONETICO_COMPANY', 'ALESIAMINCEUR');
 define('MONETICO_URL', 'https://p.monetico-services.com/test/paiement.cgi');
 define('MONETICO_RETURN_URL', 'https://www.aquavelo.com/confirmation_prix.php');
 define('MONETICO_CANCEL_URL', 'https://www.aquavelo.com/annulation_prix.php');

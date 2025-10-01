057a34ca04fbb9b36c71bdd993c38ec9444fa3c9
AQUAVELO <84918553+AQUAVELO@users.noreply.github.com>
2025-10-01 10:23:58 +0200

    Update _vente_formule.php

--- _vente_formule.php
+++ _vente_formule.php
@@ -14,9 +14,10 @@ define('MONETICO_RETURN_URL', 'https://www.aquavelo.com/confirmation.php');
 define('MONETICO_CANCEL_URL', 'https://www.aquavelo.com/annulation.php');
 
 $formules = [
-    ['nom' => 'Formule 1', 'prix' => 58.50, 'description' => 'Les 40 séances payable en 10 x 58.50 € soit la séance à 14 €. Je paie la première échéance 58.50 €.'],
-    ['nom' => 'Formule 2', 'prix' => 75, 'description' => 'Les 80 séances payable en 12 x 75 € soit la séance à 11 €. Je paie la première échéance 75 €.'],
-    ['nom' => 'Formule 3', 'prix' => 89, 'description' => 'Les 104 séances payable en 12 x 89 € soit la séance à 10 €. Je paie la première échéance 89 €.'],
+    ['nom' => 'Formule 0', 'prix' => 190, 'description' => 'Les 20 séances payable en 2 x 190 € soit la séance à 19 €. Je paie la première échéance 190 €.'],
+    ['nom' => 'Formule 1', 'prix' => 63, 'description' => 'Les 45 séances payable en 10 x 63 € soit la séance à 14 €. Je paie la première échéance 63 €.'],
+    ['nom' => 'Formule 2', 'prix' => 79, 'description' => 'Les 88 séances payable en 12 x 79 € soit la séance à 11 €. Je paie la première échéance 79 €.'],
+    ['nom' => 'Formule 3', 'prix' => 97, 'description' => 'Les 114 séances payable en 12 x 97 € soit la séance à 10 €. Je paie la première échéance 97 €.'],
     ['nom' => 'Formule 4', 'prix' => 99.00, 'description' => '12 mois illimité soit la séance à 8 € en venant 3 fois/semaine, payable en 12 x 99 €. Je paie la première échéance 99 €.']
 ];
 

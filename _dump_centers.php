2f3f09b9470bedcc5ac16dd3466d705c87160445
COURSNATATION/natation <directionalesiaminceur@gmail.com>
2026-01-18 05:38:10 +0100

    Diag centres avec exit

--- _dump_centers.php
+++ _dump_centers.php
@@ -2,5 +2,8 @@
 require '_settings.php';
 $stmt = $database->prepare("SELECT id, city FROM am_centers WHERE city LIKE '%Cannes%' OR city LIKE '%Mandelieu%' OR city LIKE '%Vallauris%'");
 $stmt->execute();
+echo "---CENTERS START---";
 print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
+echo "---CENTERS END---";
+exit;
 ?>

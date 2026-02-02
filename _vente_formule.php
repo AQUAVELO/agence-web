2090bc5f650782dca3ea059cd56b4b0e23eab53d
monpecule <84918553+AQUAVELO@users.noreply.github.com>
2026-02-02 15:09:09 +0100

    Update pricing and descriptions for Formule 5 to 7

--- _vente_formule.php
+++ _vente_formule.php
@@ -16,9 +16,10 @@ if (!defined('MONETICO_CANCEL_URL')) define('MONETICO_CANCEL_URL', 'https://www.
 $formules = [
     ['nom' => 'Formule 2', 'prix' => 112, 'description' => 'Les 27 séances à 450 € payable en 4 fois € soit la séance à 16 €, valable 9 mois. Je paie la première échéance 112 €.'],
     ['nom' => 'Formule 3', 'prix' => 63, 'description' => 'Les 45 séances payable en 10 x 63 € soit la séance à 14 €, valable 15 mois. Je paie la première échéance 63 €.'],
-    ['nom' => 'Formule 4', 'prix' => 79, 'description' => 'Les 88 séances payable en 12 x 79 € soit la séance à 11 €, valable 18 mois. Je paie la première échéance 79 €.'],
-    ['nom' => 'Formule 5', 'prix' => 97, 'description' => 'Les 114 séances en 12 x 97 € soit la séance à 10 €, valable 20 mois. Je paie la première échéance 97 €.'],
-    ['nom' => 'Formule 6', 'prix' => 99, 'description' => '12 mois illimité soit la séance à 8 € en venant 3 fois/semaine, payable en 12 x 99 €. Je paie la première échéance 99 €.']
+    ['nom' => 'Formule 5', 'prix' => 77, 'description' => 'Les 80 séances payable en 12 x 73 € soit la séance à 11 €, valable 18 mois. Je paie la première échéance 73 €.'],
+
+    ['nom' => 'Formule 6', 'prix' => 97, 'description' => 'Les 114 séances en 12 x 97 € soit la séance à 10 €, valable 20 mois. Je paie la première échéance 97 €.'],
+    ['nom' => 'Formule 7', 'prix' => 99, 'description' => '12 mois illimité soit la séance à 8 € en venant 3 fois/semaine, payable en 12 x 99 €. Je paie la première échéance 99 €.']
 ];
 
 $reference = 'CMD' . date('YmdHis') . rand(100, 999);
